5560d3d88abeb47d4bfb1dd382c8575edf0513d0
Nirpendra Chaudhary <55339698+Nirpendra09@users.noreply.github.com>
2024-10-19 06:28:24 +0000

    Project setup and adding shad cn

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+   <div>
+    <Button variant='primary'> Click</Button>
+    <Button variant="destructive">Click</Button>
+    <Button variant="outline">Click</Button>
+    <Button variant="secondary">Click</Button>
+    <Button variant="ghost">Click</Button>
+    <Button variant="teritary">Click</Button>
+   </div>
+  );
+}
